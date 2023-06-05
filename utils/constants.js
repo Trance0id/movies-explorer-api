51bb85112e39574019f945bd33f4015304be9ffe
Dmitriy Vasnev <trance0id@yandex.ru>
2023-06-05 18:05:51 +0300

    hotfix: edited cookie options

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,11 @@ const linkPattern = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-const COOKIE_OPTIONS = { maxAge: 3600000 * 24 * 7 };
-// httpOnly: true, sameSite: 'none', secure: true,
+const COOKIE_OPTIONS = {
+  maxAge: 3600000 * 24 * 7,
+  httpOnly: true,
+  sameSite: 'none',
+  secure: true,
+};
 
 module.exports = { linkPattern, DEFAULT_ALLOWED_METHODS, COOKIE_OPTIONS };
